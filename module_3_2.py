ac1f94ff35822087ef966ce67791e4fb6effb2b7
Diegosan89 <pe.scadosony@gmail.com>
2024-11-21 15:31:46 +0500

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -1,34 +1,19 @@
 # Способы вызова функции
 
-flag = False
-
-
-def comp(adr):
-    global flag
-
-    if '@' in adr and '.ru' in adr[-3:] or '.com' in adr[-4:] or '.net' in adr[-4:]:
-        flag = True
-
-    else:
-        flag = False
-
-    return flag
-
 
 def send_email(message, recipient, *, sender='university.help@gmail.com'):
-    if comp(recipient) and comp(sender):
-
-        if sender == recipient:
-            print('Нельзя отправить письмо самому себе!')
-
-        elif sender == 'university.help@gmail.com':
-            print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}.')
-
-        else:
-            print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
-
-    if not flag:
+    variants = ('.com', '.net', '.ru')
+    if '@' not in sender or '@' not in recipient or not sender.endswith(variants) or not recipient.endswith(variants):
         print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+
+    elif sender == recipient:
+        print('Нельзя отправить письмо самому себе!')
+
+    elif sender != 'university.help@gmail.com':
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
+
+    else:
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}.')
 
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
