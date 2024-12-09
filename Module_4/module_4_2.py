fdc3148c33169cc51c20fff5f653795982e83e67
Diegosan89 <pe.scadosony@gmail.com>
2024-12-09 22:04:32 +0500

    First time upload. Namespaces

---
+++ Module_4/module_4_2.py
@@ -0,0 +1,9 @@
+# Пространство имен
+
+def test_function():
+    def inner_function():
+        print('Я в области видимости функции test_function')
+
+    inner_function()
+
+inner_function() # name 'inner_function' is not defined
